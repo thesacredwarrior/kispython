a96c90fabb4251e20c5b1135805081f19bd5bed5
wakeuplikethis <79377228+wakeuplikethis@users.noreply.github.com>
2022-04-29 15:15:22 +0300

    Add files via upload

---
+++ numberTwo.py
@@ -0,0 +1,13 @@
+from math import *
+
+
+def main(z):
+    if z < 34:
+        return pow(1 + 33 * z ** 2, 4) - pow((1 - z - z ** 3), 7)
+    if 34 <= z < 73:
+        return (59 * pow(z, 3)) - 0.03 - (20 * z) + (77 * pow(tan(z), 7))
+    else:
+        return 97 * pow(36 * z, 4) + pow((z ** 2 + 50 + z ** 3), 2) / 94
+
+
+print(main(62))
