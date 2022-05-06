0e06e1f7756fe98859ba0872071e4a3da1a96b75
unknown9382931 <79377228+wakeuplikethis@users.noreply.github.com>
2022-05-07 01:12:56 +0300

    Update numberFive.py

--- numberFive.py
+++ numberFive.py
@@ -9,3 +9,20 @@ def main(y):
     return number
 
 print(main([-0.01, 0.88, 0.37, -0.69]))
+
+
+import math
+
+
+
+def main(y):
+    sum = 0
+    for i in range(len(y)):
+        dif1 = (pow(y[1 - math.trunc(i/3)], 2) / 55)
+        dif2 = 93 * y[math.trunc(i/4)]
+        dif3 = pow(y[math.trunc(i/4)], 3)
+        sub_res = (dif1 - dif2 - dif3)
+        sum += 28 * pow(sub_res, 4)
+    return sum
+
+print(main([-0.01, 0.88, 0.37, -0.69]))
