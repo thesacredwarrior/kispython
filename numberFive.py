b1dae415d8015243d81e5a6111285799822c4f9a
Life Break <79377228+thesacredwarrior@users.noreply.github.com>
2022-05-23 20:47:11 +0300

    Update numberFive.py
    done

--- numberFive.py
+++ numberFive.py
@@ -1,28 +1,15 @@
-from math import ceil
-
-
-def main(y):
-    number = 0
-    n = len(y)
-    for i in range(1, n + 1):
-        number += 28 * (((y[ceil(i / 3) - 1] ** 2 / 55) - (93 * y[ceil(i / 4) - 1]) - (y[ceil(i / 4) - 1] ** 3)) ** 4)
-    return number
-
-print(main([-0.01, 0.88, 0.37, -0.69]))
-
-
 import math
 
 
-
 def main(y):
-    sum = 0
-    for i in range(len(y)):
-        dif1 = (pow(y[1 - math.trunc(i/3)], 2) / 55)
-        dif2 = 93 * y[math.trunc(i/4)]
-        dif3 = pow(y[math.trunc(i/4)], 3)
-        sub_res = (dif1 - dif2 - dif3)
-        sum += 28 * pow(sub_res, 4)
-    return sum
+    n = len(y)
+    y.insert(0, 0)
+    total = 0
+    for i in range(1, n + 1):
+        left = (y[n + 1 - math.ceil(i / 3)] ** 2) / 55
+        middle = 93 * y[math.ceil(i / 4)]
+        right = (y[math.ceil(i / 4)]) ** 3
+        total += (left - middle - right) ** 4
+    return 28 * total
 
 print(main([-0.01, 0.88, 0.37, -0.69]))
