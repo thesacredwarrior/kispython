e1b5f994e9508f6744aa9fa092a2d04dddc64809
Life Break <79377228+thesacredwarrior@users.noreply.github.com>
2022-05-26 05:01:52 +0300

    Create task8.py

---
+++ task8.py
@@ -0,0 +1,21 @@
+import re
+
+
+def main(str):
+    pattern = "(?<=[@]).*?(?=[.][|][|])"
+    result = {}
+
+    str = str.replace(" ", "")
+    str = str.replace("\n", "")
+    access = re.findall(pattern, str)
+
+    for i in access:
+        txt = i.split("=>")
+        result[txt[1]] = txt[0]
+
+    return result
+
+str = ".begin ||define @'erlage'=>geti_654. |||| define @'maer_9'=> biti. |||| define @'tienza' => edtile_135. || || " \
+      "define @'lain' => adidixe.||.end"
+
+print(main(str))
